eb29e597f8dcbc451fdf782357ecb92f4048b215
Davey <meeprdib@outlook.com>
2025-05-12 01:41:59 -0400

    kube tuah define on that thang

--- kubejs/server_scripts/recipes/material_definitions.js
+++ kubejs/server_scripts/recipes/material_definitions.js
@@ -11,15 +11,15 @@ let raw_material_definition = (
 ) => {};
 
 raw_material_definition(
-  "#forge:ores/gold",
-  "minecraft:raw_gold",
-  "minecraft:raw_gold_block",
-  "minecraft:gold_ingot",
-  "minecraft:gold_block",
-  "minecraft:gold_nugget",
-  "tconstruct:molten_gold_bucket",
-  "tconstruct:molten_gold",
-  "crossroads:dust_gold"
+  "#forge:ores/silver",
+  "galosphere:raw_silver",
+  "galosphere:raw_silver_block",
+  "galosphere:silver_ingot",
+  "galosphere:silver_block",
+  "galosphere:silver_nugget",
+  "tconstruct:molten_silver_bucket",
+  "tconstruct_molten_silver",
+  "kubejs:silver_dust"
 );
 
 raw_material_definition(
@@ -32,6 +32,54 @@ raw_material_definition(
   "tconstruct:molten_iron_bucket",
   "tconstruct:molten_iron",
   "crossroads:dust_iron"
+);
+
+raw_material_definition(
+  "#forge:ores/copper",
+  "minecraft:raw_copper",
+  "minecraft:raw_copper_block",
+  "minecraft:copper_ingot",
+  "minecraft:copper_block",
+  'tconstruct:copper_nugget',
+  "tconstruct:molten_copper_bucket",
+  "tconstruct:molten_copper",
+  "crossroads:dust_copper"
+);
+
+raw_material_definition(
+  "#forge:ores/lead",
+  "oreganized:raw_lead",
+  "oreganized:raw_lead_block",
+  "oreganized:lead_ingot",
+  "oreganized:lead_block",
+  "oreganized:lead_nugget",
+  "tconstruct:molten_lead_bucket",
+  "tconstruct:molten_lead",
+  "kubejs:lead_dust"
+);
+
+raw_material_definition(
+  "#forge:ores/gold",
+  "minecraft:raw_gold",
+  "minecraft:raw_gold_block",
+  "minecraft:gold_ingot",
+  "minecraft:gold_block",
+  "minecraft:gold_nugget",
+  "tconstruct:molten_gold_bucket",
+  "tconstruct:molten_gold",
+  "crossroads:dust_gold"
+);
+
+raw_material_definition(
+  "#forge:ores/tin",
+  "crossroads:raw_tin",
+  "crossroads:block_raw_tin",
+  "crossroads:ingot_tin",
+  "crossroads:block_tin",
+  "crossroads:nugget_tin",
+  "tconstruct:molten_tin_bucket",
+  "tconstruct:molten_tin",
+  "crossroads:dust_tin"
 );
 
 raw_material_definition(
--- kubejs/startup_scripts/items.js
+++ kubejs/startup_scripts/items.js
@@ -1,3 +1,6 @@
 StartupEvents.registry('item', event => {
   event.create('cobalt_dust')
+  event.create('lead_dust')
+  event.create('silver_dust')
+  event.create('tin_dust')
 })
