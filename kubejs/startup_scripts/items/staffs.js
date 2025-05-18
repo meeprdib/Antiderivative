8bb3ce3be4309490f0aa9dd319747e390497eedd
Davey <meeprdib@outlook.com>
2025-05-17 23:58:36 -0400

    ender and teleportation staffs

--- kubejs/startup_scripts/items/staffs.js
+++ kubejs/startup_scripts/items/staffs.js
@@ -2,4 +2,13 @@ StartupEvents.registry('item', event => {
   event.create('fire_staff', "irons_spells_js:staff")
   .modelJson(JsonIO.read('kubejs/assets/kubejs/models/item/fire_staff.json'))
   .rarity("traveloptics:ignis")
+
+  event.create('ender_staff', "irons_spells_js:staff")
+  .modelJson(JsonIO.read('kubejs/assets/kubejs/models/item/ender_staff.json'))
+  .rarity("traveloptics:void")
+
+event.create('teleportation_staff', "irons_spells_js:staff")
+  .modelJson(JsonIO.read('kubejs/assets/kubejs/models/item/teleportation_staff.json'))
+  .modifyAttribute("additional_attributes:innate_spell/irons_spellbooks/teleport", "Teleport", 1.0, "addition")
+  .rarity("traveloptics:void")
 })
