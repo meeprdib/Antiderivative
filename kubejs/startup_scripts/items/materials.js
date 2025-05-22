01041ec40a5840423ffdbcd48da87906da09c5b5
Davey <meeprdib@outlook.com>
2025-05-21 20:05:30 -0400

    new grits and dusts

--- kubejs/startup_scripts/items/materials.js
+++ kubejs/startup_scripts/items/materials.js
@@ -2,4 +2,8 @@ StartupEvents.registry("item", (event) => {
   event.create("cobalt_dust");
   event.create("lead_dust");
   event.create("silver_dust");
+
+  event.create("cobalt_grit");
+  event.create("lead_grit");
+  event.create("silver_grit");
 });
