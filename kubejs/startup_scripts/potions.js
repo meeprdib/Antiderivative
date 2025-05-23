b8d7d2eedd3b70e14788ca570d6b441cae15d01e
Davey <meeprdib@outlook.com>
2025-05-23 01:02:55 -0400

    potion registration function

--- kubejs/startup_scripts/potions.js
+++ kubejs/startup_scripts/potions.js
@@ -1,15 +1,27 @@
-StartupEvents.registry('potion', event=>{
-   let potionBuilder =  Java.loadClass('dev.latvian.mods.kubejs.misc.PotionBuilder')
-   let effect = `irons_spellbooks:angel_wings`
- 
-   event.createCustom('kubejs:angel_wings', ()=>{
-      return new potionBuilder(`irons_spellbooks:angel_wings`)
-      .effect(effect, 200, 0)
-      .createObject()
-   })
-   event.createCustom('kubejs:long_angel_wings', ()=>{
-      return new potionBuilder(`irons_spellbooks:angel_wings`)
-      .effect(effect, 600, 0)
-      .createObject()
-   })
-})
+StartupEvents.registry("potion", (event) => {
+  let potionBuilder = Java.loadClass("dev.latvian.mods.kubejs.misc.PotionBuilder");
+
+  let potionRegistration = (effect, name, length, amplification) => {
+    event.createCustom(`kubejs:${name.toLowerCase().replace(/\s+/g, "_")}`, () => {
+      return new potionBuilder(effect)
+        .effect(effect, length, amplification)
+        .createObject();
+    });
+  };
+
+  potionRegistration(`irons_spellbooks:angel_wings`, 'Angel Wings', 3600, 0);
+  potionRegistration(`irons_spellbooks:angel_wings`, 'Long Angel Wings', 9600, 0);
+});
+
+StartupEvents.registry("mob_effect", (event) => {
+  event
+    .create("fortune")
+    .color(0x000000)
+    .beneficial()
+    .modifyAttribute(
+      "tinkersjewelry:fortune",
+      "e0f4e796-3d3d-11ee-be56-0242ac183754",
+      1,
+      "addition"
+    );
+});
