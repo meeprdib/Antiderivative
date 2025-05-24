33fc7ffb3f93171273973cd5490444d0e2026c37
Davey <meeprdib@outlook.com>
2025-05-23 21:36:13 -0400

    got bored of tormenting deepa

--- kubejs/server_scripts/recipes/recipes.js
+++ kubejs/server_scripts/recipes/recipes.js
@@ -1,1 +1,26 @@
-ServerEvents.recipes(event => {event.shaped('minecraft:hopper', ['A A', 'ABA', ' A '], {A: '#forge:ingots/tin', B: '#forge:chests/wooden'});event.shaped('minecraft:minecart', ['A A', 'AAA'], {A: '#forge:ingots/tin'});event.shaped('16x refinedstorage:cable', ['AAA', 'BCB', 'AAA'], {A: '#forge:ingots/tin', B: '#oreganized:crystal_glass', C: '#forge:dusts/redstone'});event.shaped('refinedstorage:external_storage', ['ABA', 'CDC', 'ABA'], {A: '#forge:nuggets/slimesteel', B: '#forge:ingots/tin', C: '#storagedrawers:drawers', D: 'refinedstorage:cable'});event.shapeless('refinedstorage:importer', ['#forge:nuggets/slimesteel', 'refinedstorage:cable', 'minecraft:hopper']);event.shapeless('refinedstorage:exporter', ['refinedstorage:importer'])})
+ServerEvents.recipes((event) => {
+  event.shaped("minecraft:hopper", ["A A", "ABA", " A "], {
+    A: "#forge:ingots/tin",
+    B: "#forge:chests/wooden",
+  });
+  event.shaped("minecraft:minecart", ["A A", "AAA"], {
+    A: "#forge:ingots/tin",
+  });
+  event.shaped("16x refinedstorage:cable", ["AAA", "BCB", "AAA"], {
+    A: "#forge:ingots/tin",
+    B: "#oreganized:crystal_glass",
+    C: "#forge:dusts/redstone",
+  });
+  event.shaped("refinedstorage:external_storage", ["ABA", "CDC", "ABA"], {
+    A: "#forge:nuggets/slimesteel",
+    B: "#forge:ingots/tin",
+    C: "#storagedrawers:drawers",
+    D: "refinedstorage:cable",
+  });
+  event.shapeless("refinedstorage:importer", [
+    "#forge:nuggets/slimesteel",
+    "refinedstorage:cable",
+    "minecraft:hopper",
+  ]);
+  event.shapeless("refinedstorage:exporter", ["refinedstorage:importer"]);
+});
